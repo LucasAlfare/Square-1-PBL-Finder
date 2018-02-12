0e53d177e358c33a59d29435620cdc23043c8f80
Lucas <god-lucas@hotmail.com>
2018-02-11 21:22:28 -0300

    some commenting and cleans

--- src/com/main/pbl/Finder.java
+++ src/com/main/pbl/Finder.java
@@ -20,47 +20,43 @@ public class Finder {
         sucessSearches = new ArrayList<>();
     }
 
-    public void search(){
-//        SquareOne squareOne = new SquareOne();
-
+    public void search() {
         Cube squareOne = new Cube();
         squareOne.applyStringSequence(reversedSequence(targetPbl.getTopPLL().getSequence()));
         squareOne.applyStringSequence(reversedSequence(targetPbl.getBottomPLL().sequenceAtBottom()));
 
         setups = "Setups aplicados:\n";
         setups += reversedSequence(targetPbl.getTopPLL().getSequence()) + ";\n" +
-                reversedSequence(targetPbl.getBottomPLL().sequenceAtBottom())+ ";\n\n";
+                reversedSequence(targetPbl.getBottomPLL().sequenceAtBottom()) + ";\n\n";
 
         long i = System.currentTimeMillis();
+        for (AuxAlg a : AlgTemplates.AUX_ALGS) {
+            for (AuxAlg b : AlgTemplates.AUX_ALGS) {
+                searching = true;
 
-//        Thread a = new Thread(new Runnable() {
-//            @Override
-//            public void run() {
-                for (AuxAlg a : AlgTemplates.AUX_ALGS) {
-                    for (AuxAlg b : AlgTemplates.AUX_ALGS) {
-                        searching = true;
+                String testSolveSeq = a.getSequence() + b.getSequence();
+                squareOne.applyStringSequence(testSolveSeq);
 
-                        String testSolveSeq = a.getSequence() + b.getSequence();
-                        squareOne.applyStringSequence(testSolveSeq);
-
-                        if (isSolved(squareOne)) {
-                            elapsedTime = System.currentTimeMillis() - i;
-                            sucessSearches.add(new SucessSearch(targetPbl, testSolveSeq, a, b));
-                            squareOne.applyStringSequence(reversedSequence(testSolveSeq));
-                        } else {
-                            squareOne.applyStringSequence(reversedSequence(testSolveSeq));
-                        }
-                    }
+                if (isSolved(squareOne)) {
+                    elapsedTime = System.currentTimeMillis() - i;
+                    sucessSearches.add(new SucessSearch(targetPbl, testSolveSeq, a, b));
+                    squareOne.applyStringSequence(reversedSequence(testSolveSeq));
+                } else {
+                    squareOne.applyStringSequence(reversedSequence(testSolveSeq));
                 }
-//            }
-//        });
-//        a.start();
+            }
+        }
 
         searching = false;
         elapsedTime = System.currentTimeMillis() - i;
     }
 
-    //TODO: verificar faces em apenas 1 loop
+    /**
+     * Checks if both layers of passed square-1 are solved.
+     *
+     * @param targetSquare the square to check layers.
+     * @return true if solved.
+     */
     private boolean isSolved(Cube targetSquare) {
         Cube solved = new Cube();
         ArrayList<Byte> topSolvedBytes = new ArrayList<>();
@@ -75,12 +71,24 @@ public class Finder {
                 (Collections.indexOfSubList(bottomSolvedBytes, Arrays.asList(targetSquare.getPieces(false))) != -1);
     }
 
+    /**
+     * Reverses a square-1 sequence.
+     * Example:
+     * original ->  (1,-3)/
+     * out      -> /(-1,3)
+     *
+     * @param algorithm original sequence do be reversed.
+     * @return a reversed string from original param.
+     */
     private String reversedSequence(String algorithm) {
         ArrayList<String> hold = new ArrayList<>();
+        //cleans and splits
         ArrayList<String> aux = new ArrayList<>(Arrays.asList(algorithm.replaceAll(" ", "").split("/")));
 
+        //reverses
         Collections.reverse(aux);
 
+        //inverses
         for (String x : aux) {
             String[] aux2 = x.split(",");
             if (!x.equals("")) {
@@ -88,14 +96,21 @@ public class Finder {
             }
         }
 
+        //cleans toString list
         String r = hold.toString().replaceAll(", ", "/").replaceAll("\\[", "").replaceAll("]", "").replaceAll(" ", "");
 
+        //readds twists
         if (algorithm.startsWith("/")) r += "/";
         if (algorithm.endsWith("/")) r = "/" + r;
 
         return r;
     }
 
+    /**
+     * USELESS.
+     *
+     * @return
+     */
     @Deprecated
     public PBL[] allPbls() {
         ArrayList<PBL> r = new ArrayList<>();
